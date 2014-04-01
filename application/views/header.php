8c23eeab37cbe23d053f0ba3e0276e0ca265e279
Shubhajit Saha <shubhajit@iitb.ac.in>
2014-04-02 02:28:38 +0530

    logo added

--- application/views/header.php
+++ application/views/header.php
@@ -8,7 +8,9 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="<?php echo base_url(); ?>">String Your Story</a>
+        <a class="navbar-brand" href="<?php echo base_url(); ?>">
+          <img src="<?php echo base_url('assets/img/logo.png'); ?>" width="120">
+        </a>
       </div>
 
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
