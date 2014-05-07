8a021b638a21f06e44e81d9e07da2ee75ae02b3e
Shubhajit Saha <shubhajit@iitb.ac.in>
2014-05-07 20:44:09 +0530

    Facebook like button proper link

--- application/views/footer.php
+++ application/views/footer.php
@@ -7,7 +7,7 @@
       </div>
 
       <div class="col-xs-6">
-        <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80&amp;appId=395123650630353" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>
+        <iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FString-Your-Story%2F740947862594212%2F&amp;width&amp;layout=standard&amp;action=like&amp;show_faces=true&amp;share=true&amp;height=80&amp;appId=395123650630353" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:80px;" allowTransparency="true"></iframe>
       </div>
 
     </div>
